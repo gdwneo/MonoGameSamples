65ab390110b3a6c93e977701ef39708ff74f13e3
Aybe <aybe.one@hotmail.com>
2015-04-17 19:51:21 +0200

    First commit

---
+++ MonoGameSample2dOrtho/Game1.cs
@@ -0,0 +1,68 @@
+﻿// This sample shows how to draw 2D lines
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameSample2dOrtho
+{
+    public class Game1 : Game
+    {
+        private BasicEffect _basicEffect;
+        private VertexPositionColor[] _colors;
+        private GraphicsDeviceManager _graphics;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            _colors = new[]
+            {
+                new VertexPositionColor(new Vector3(0.0f, 0.0f, 0.0f), Color.Red),
+                new VertexPositionColor(new Vector3(100.0f, 0.0f, 0.0f), Color.Green),
+                new VertexPositionColor(new Vector3(100.0f, 100.0f, 0.0f), Color.Blue),
+                new VertexPositionColor(new Vector3(0.0f, 100.0f, 0.0f), Color.Yellow),
+                new VertexPositionColor(new Vector3(0.0f, 0.0f, 0.0f), Color.Red)
+            };
+            _basicEffect = new BasicEffect(GraphicsDevice) {VertexColorEnabled = true};
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _basicEffect.World = // NOTE: the correct order for matrices is SRT (scale, rotate, translate)
+                Matrix.CreateTranslation(0.5f, 0.5f, 0.0f)* // offset by half pixel to get pixel perfect rendering
+                Matrix.CreateTranslation(100.0f, 100.0f, 0.0f)* // draw at 100, 100
+                Matrix.CreateOrthographicOffCenter // 2d projection
+                    (
+                        0.0f,
+                        GraphicsDevice.Viewport.Width, // NOTE : here not an X-coordinate (i.e. width - 1)
+                        GraphicsDevice.Viewport.Height,
+                        0.0f,
+                        0.0f,
+                        1.0f
+                    );
+
+            var passes = _basicEffect.CurrentTechnique.Passes;
+            foreach (var pass in passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives(PrimitiveType.LineStrip, _colors, 0, _colors.Length - 1);
+            }
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoGameSample3dTriangle/Game1.cs
@@ -0,0 +1,68 @@
+﻿// This samples draws a triangle in a 3D space
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameSample3dTriangle
+{
+    public class Game1 : Game
+    {
+        private readonly GraphicsDeviceManager _graphics;
+        private BasicEffect _basicEffect;
+        private VertexPositionColor[] _colors;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            _colors = new[]
+            {
+                new VertexPositionColor(new Vector3(0.0f, +0.5f, 0.0f), Color.Red),
+                new VertexPositionColor(new Vector3(+0.5f, -0.5f, 0.0f), Color.Green),
+                new VertexPositionColor(new Vector3(-0.5f, -0.5f, 0.0f), Color.Blue)
+            };
+
+            _basicEffect = new BasicEffect(GraphicsDevice) {VertexColorEnabled = true};
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // rotate our triangle continuously
+            var angle = MathHelper.ToRadians((float) gameTime.TotalGameTime.TotalMilliseconds/10.0f);
+            _basicEffect.World = Matrix.CreateRotationY(angle);
+
+            // look at it, step back a bit
+            _basicEffect.View = Matrix.CreateLookAt(new Vector3(0.0f, 0.0f, 2.0f), Vector3.Zero, Vector3.Up);
+
+            // we need a projection too, here 45 degrees
+            _basicEffect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f),
+                _graphics.GraphicsDevice.Viewport.AspectRatio, 0.1f, 100.0f);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // to simplify here,
+            // we want to see our triangle no matter where we look at it from
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            foreach (var pass in _basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, _colors, 0, _colors.Length/3);
+            }
+            base.Draw(gameTime);
+        }
+    }
+}
